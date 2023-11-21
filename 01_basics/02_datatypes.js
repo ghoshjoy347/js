f147208968a569457b66e13fbec7df8be97a3eb0
ghoshjoy347 <145364240+ghoshjoy347@users.noreply.github.com>
2023-11-21 09:35:07 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let name = "Joy" //string
+let age = 22 //number
+let isLoggedIn = true //boolean
+let state = null //empty 
+//undefined means till now data haven't been uploaded
+//symbols -> uniqueness
+
+console.log(typeof "joy");//it shows the datatype which we have written within "...."
+console.log(typeof undefined);//the datatype of undefined is undefined
+console.log(typeof null);//the datatype of null is object
