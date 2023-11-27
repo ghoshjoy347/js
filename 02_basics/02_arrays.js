99d74b97e7432fd03194a9d37f9ffd2ccc0f4685
ghoshjoy347 <145364240+ghoshjoy347@users.noreply.github.com>
2023-11-27 23:13:40 +0000

    arrays

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,6 @@
+const marvel = ["hulk","ironman","thor"]
+const dc = ["superman","batman","joker"]
+
+marvel.push(dc)
+
+console.log(marvel)//it gives the output as an array inside a array -> [ 'hulk', 'ironman', 'thor', [ 'superman', 'batman', 'joker' ] ]
