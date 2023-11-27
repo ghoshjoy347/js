ca4a52142719850485753bbdd71ec18b469a1f91
ghoshjoy347 <145364240+ghoshjoy347@users.noreply.github.com>
2023-11-27 23:41:22 +0000

    arrays

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,6 +1,12 @@
 const marvel = ["hulk","ironman","thor"]
 const dc = ["superman","batman","joker"]
 
-marvel.push(dc)
+// marvel.push(dc)
 
-console.log(marvel)//it gives the output as an array inside a array -> [ 'hulk', 'ironman', 'thor', [ 'superman', 'batman', 'joker' ] ]
+// console.log(marvel)//it gives the output as an array inside a array -> [ 'hulk', 'ironman', 'thor', [ 'superman', 'batman', 'joker' ] ]
+
+const hero = marvel.concat(dc)//concatenate 2 arrays together into a single array
+console.log(hero)
+
+const newhero = [...marvel,...dc]//spread operator , works same like concatenate
+console.log(newhero)
