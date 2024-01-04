77a8f7ce21b2251a6a0edba0d352c69024f7a325
ghoshjoy347 <145364240+ghoshjoy347@users.noreply.github.com>
2024-01-04 16:13:04 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,44 @@
+
+function sayMyName(){ //this is a function and we can call it whenever it is required
+    console.log("A")
+    console.log("B")
+    console.log("C")
+    console.log("D")
+    console.log("E")
+    console.log("F")
+    console.log("G")
+    
+}
+
+//sayMyName - it is called as reference
+//sayMyName()//execution
+
+//function addTwoNumbers(num1,num2){  ////(num1,num2)within brackets those values are known as "parameters"
+//    console.log(num1 + num2)
+//}
+
+// function addTwoNumbers(num1,num2){ 
+
+// console.log(num1 + num2)
+// }
+
+// Answers -
+// 8
+// Result :  undefined
+
+function addTwoNumbers(num1,num2){ 
+
+    return num1 + num2
+    console.log("xyz") //it won't be printed as it is after return result as after return it cannot be executed,but if we wrote before return then the command would be printed
+    }
+const result = addTwoNumbers(3, 5) //(3,5)within brackets those values are known as "argument"
+//console.log("Result : ", result)
+// addTwoNumbers(3,"8")
+// addTwoNumbers(3,"a")
+
+function loginUserMessage(username) {
+
+    return (username + " just logged in")
+    return `${username} just logged in `
+}
+console.log(loginUserMessage('ghosh'))
