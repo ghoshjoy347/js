d200717b70ba90b20a76274cba47fbf127f6a1eb
ghoshjoy347 <145364240+ghoshjoy347@users.noreply.github.com>
2024-01-07 16:57:06 +0000

    truthy and falsy

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -26,3 +26,23 @@ const emptyObj = {}
 if(Object.keys(emptyObj).length === 0){
     console.log("Object is Empty")
 }
+
+// Nullish Coalescing Operator (??): null undefined
+// The above operator is created for null and undefined ,suppose if null/undefined values comes then it would affect the program or won't go further but with Nullish Coalescing Operator, it would check whether it contains value or simply null or undefined,if found null/undefined then it would go to the next value that is assigned there.
+
+
+let value 
+
+//value = 5 ?? 10
+//value = null ?? 10 //always it takes the first value after null/undefined
+//value = undefined ?? 10
+
+console.log(value)
+
+
+// Ternary Operator
+
+// condition ? true : false
+
+const price = 79
+price < 80 ? console.log("less than 80") : console.log("greater than 80")
