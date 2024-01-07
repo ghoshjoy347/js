2d3a68002b8d2cf20bc037432d4856cca95f0bed
ghoshjoy347 <145364240+ghoshjoy347@users.noreply.github.com>
2024-01-07 16:02:06 +0000

    switchcases

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,45 @@
+
+const month = 8  //shift + alt + down arrow (copy and paste chuck of code below)
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("apri");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("august");
+        break;
+    case 9:
+        console.log("sept");
+        break;
+    case 10:
+        console.log("oct");
+        break;
+    case 11:
+        console.log("nov");
+        break;
+    case 12:
+        console.log("dec");
+        break;
+        
+    default:
+        break;
+}
+
