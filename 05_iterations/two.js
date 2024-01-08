37464688cab8615e135fe8d55153feab9ebd181d
ghoshjoy347 <145364240+ghoshjoy347@users.noreply.github.com>
2024-01-08 15:24:49 +0000

    loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,14 @@
+
+// let i = 0
+// while (i <= 10) {
+//   console.log(`value of index i is ${i}`)
+//   i = i + 2
+  
+// }
+
+let arr = ["joy","ram","sam"]
+start = 0
+while(start < arr.length){
+  console.log(`Value is ${arr[start]}`)
+  start ++
+}
