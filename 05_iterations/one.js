22d74fc930fdad9f1fdc697ca68a6d2045ac0bc5
ghoshjoy347 <145364240+ghoshjoy347@users.noreply.github.com>
2024-01-08 11:02:38 +0000

    iterations

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -14,8 +14,28 @@
 //     }
 // }
 
-let arr = ["abc","mno","xyz"]
-for(let i = 0; i < arr.length ; i++){
-    const element = arr[i]
-    console.log(element);
+// let arr = ["abc","mno","xyz"]
+// for(let i = 0; i < arr.length ; i++){
+//     const element = arr[i]
+//     console.log(element);
+// }
+
+//break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//   if(index == 5){
+//     console.log(`detected 5`);
+//     break
+//   }
+//   console.log(`value of index is ${index}`);
+  
+// }
+
+for (let index = 1; index <= 20; index++) {
+  if(index == 5){
+    console.log(`detected 5`);
+    continue
+  }
+  console.log(`value of index is ${index}`);
+  
 }
