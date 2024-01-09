88d4cb46c039678dbb3c95ed9f909b0bb644fda7
ghoshjoy347 <145364240+ghoshjoy347@users.noreply.github.com>
2024-01-09 11:34:14 +0000

    iterations

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -8,9 +8,16 @@ const values = prg.forEach((item) => { //forEach doesn't retuns values always it
 
 console.log(values)
 
-const myNum = [1,2,3,4,5,6,7,8,9]
+//const myNum = [1,2,3,4,5,6,7,8,9]
 
-const newNum = myNum.filter((num) => num > 4)
-    console.log(newNum);
+//const newNum = myNum.filter((number) => number > 4) // "filter" returns the value, whereas "forEach" doesn't returns value // ((number) => number > 4) -> this is called call back function, and then a condition (number > 4), those value which satisfies the condition those will return the values
+    //console.log(newNum);
 
-    //8:25:00
+// if we start a scope,i.e, { } then we have to use return keyword without return keyword it would give empty array. Check below
+
+const myNum2 = [1,2,3,4,5,6,7,8,9]
+
+const newNum2 = myNum2.filter((number) => {
+    return number > 4 // here, using "filter" is easy as compared to "forEach"
+})
+    console.log(newNum2);
