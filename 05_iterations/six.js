20b38e299f59d88d73c718e68c933985d8c10bd3
ghoshjoy347 <145364240+ghoshjoy347@users.noreply.github.com>
2024-01-09 13:36:31 +0000

    filters

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -1,12 +1,12 @@
 
-const prg = ["js","java","cpp","python"]
+// const prg = ["js","java","cpp","python"]
 
-const values = prg.forEach((item) => { //forEach doesn't retuns values always it would show undefined
-    console.log(item)
-    return item
-})
+// const values = prg.forEach((item) => { //forEach doesn't retuns values always it would show undefined
+//     console.log(item)
+//     return item
+// })
 
-console.log(values)
+// console.log(values)
 
 //const myNum = [1,2,3,4,5,6,7,8,9]
 
@@ -15,9 +15,23 @@ console.log(values)
 
 // if we start a scope,i.e, { } then we have to use return keyword without return keyword it would give empty array. Check below
 
-const myNum2 = [1,2,3,4,5,6,7,8,9]
+// const myNum2 = [1,2,3,4,5,6,7,8,9]
 
-const newNum2 = myNum2.filter((number) => {
-    return number > 4 // here, using "filter" is easy as compared to "forEach"
-})
-    console.log(newNum2);
+// const newNum2 = myNum2.filter((number) => {
+//     return number > 4 // here, using "filter" is easy as compared to "forEach"
+// })
+//     console.log(newNum2);
+
+const books = [
+    {name: "Atomic Habits", genre: "Lifestyle", edition: "2004", publish: "2000" },
+    {name: "Psychology of Money", genre: "Finance", edition: "2010", publish: "2001" },
+    {name: "Rich Dad Poor Dad", genre: "Finance", edition: "2007", publish: "2002"  },
+    {name: "Kabuliwala", genre: "Adventure", edition: "1990", publish: "1980"  },
+    {name: "Seven Seas", genre: "Action", edition: "2015", publish: "2003"  },
+]
+
+let userBooks = books.filter( (name) => name.genre === "Adventure")
+
+userBooks = books.filter((name) => name.publish > "2000" && name.genre == "Finance")
+
+console.log(userBooks)
