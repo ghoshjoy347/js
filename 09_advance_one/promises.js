8709322771c08a0bc16fb9a440c6438defe3d4cb
ghoshjoy347 <145364240+ghoshjoy347@users.noreply.github.com>
2024-01-18 15:34:41 +0000

    promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -14,14 +14,14 @@ setTimeout(function(){
 })
 
 
-promiseOne.then(function(){
-    console.log("Promise Consumed");
+promiseOne.then(function(){      // consumption of promise
+    console.log("Promise Consumed"); // '.then' -> is directly related to 'resolve' not others, we get a callback function inside '.then' 
 })
 
 new Promise(function(resolve,reject){
     setTimeout(function(){
         console.log("Async task 2")
-        resolve()
+        resolve()                   // resolve() is a method which needs to be called, it helps in connecting ".then" with 'resolve'
     })
 }).then(function(){
     console.log("Async 2 solved");
@@ -35,22 +35,63 @@ const promiseThree =  new Promise(function(resolve,reject){
 })
 
 promiseThree.then(function(user){
-console.log(user);
+console.log(user); // we get all the parameters, that is passed from resolve
 })
 
 
 
 const promiseFour = new Promise(function(resolve,reject){
     setTimeout(function(){
-        let error = true
-        if(error != 0){
-            resolve({username: "joy", password: "123"})
+        let error = true // if false it prints 'joyop', and if true it prints 'Something went wrong'
+        if(!error){
+            resolve({username: "joyop", password: "123"})
         }
         else{
             reject("Something went wrong")
         }
-    },100)
+    },1000)
 })
 
-promiseFour.then().catch()
-//5:50:20
+promiseFour
+.then((user) => {
+    console.log(user)
+    return user.username
+})
+.then((username) => {                   // it's called chaining,it gets the returned value from the previous ".then"
+    console.log(username)
+})
+.catch(function(error){              //we need to use catch(), or else it would throw a error
+    console.log(error);
+})
+.finally(() => {              //we need to use catch(), or else it would throw a error
+    console.log("The promise is resolved or rejected");
+})
+
+
+
+const promiseFive = new Promise(function(resolve,reject){  // we can also use arrow function here
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({username: "JAvascript", password: "123"})
+        }
+        else{
+            reject("JS went wrong")
+        }
+    },1000)
+}) 
+
+async function consumePromiseFive(){  //async await has an issue that, it cannot directly handle the errors
+    try {
+        
+        const response = await promiseFive
+        console.log(response)
+
+    } catch (error) {
+        
+        console.log(error)
+
+    }
+}
+
+consumePromiseFive()
