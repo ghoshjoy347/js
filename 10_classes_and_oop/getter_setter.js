fd2b13e2e7b5ba0b4ed060710958ad87e71297b6
ghoshjoy347 <145364240+ghoshjoy347@users.noreply.github.com>
2024-01-21 17:28:25 +0000

    getter and setter

--- 10_classes_and_oop/getter_setter.js
+++ 10_classes_and_oop/getter_setter.js
@@ -4,14 +4,23 @@ class User {
         this.password = password
     }
 
+    get email(){             
+        return this._email.toUpperCase()
+    }
+
+    set email(value){        
+        this._email = value
+    }
+
+
     get password(){             //getter - if someone wants to access/get the value then the value given
         return this._password.toUpperCase()
     }
 
-    set password(value){        //setters - setting this value
+    set password(value){        //setters - setting this value and we do not give return type in setters
         this._password = value
     }
 }
-
 const joy = new User("joy@ai","abc")
-console.log(joy.password);
+//console.log(joy.password)
+console.log(joy.email)
